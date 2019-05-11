dcaaf2de525b6a8074a21887f2c3fdddab01d81f
nikitabelotelov <nikbelotelov@mail.ru>
2019-05-11 19:05:29 +0300

    try manager panel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './normalize.css';
+import {Panel} from './ManagerPanel/Panel'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                START PAGE
+                <Router>
+                    <Panel/>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,7 +1,16 @@
+import ACTION_TYPES from '../actions/actionTypes';
+
 const initialState = {
-    settings: []
+    settings: ['huisosi']
 };
-function rootReducer(state:any = initialState, action:any) {
+
+function rootReducer(state: any = initialState, action: any) {
+    if (action.type === ACTION_TYPES.addSetting) {
+        return Object.assign({}, state, {
+            settings: state.settings.concat(action.payload)
+        });
+    }
     return state;
 };
+
 export default rootReducer;
