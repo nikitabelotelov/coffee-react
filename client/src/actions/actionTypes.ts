bba06a71eb19bfd69771607bef34bf4555f6f529
Zuev Dmitrii <xakkez@gmail.com>
2019-10-20 18:11:51 +0200

    move files

--- client/src/actions/actionTypes.ts
+++
@@ -1,9 +0,0 @@
-let ACTION_TYPES = {
-    setSetting: "setSetting",
-    setMachineState: "setMachineState",
-    currentInfoUpdate: "currentInfoUpdate",
-    initialSettings: "initialSettings",
-    updateSettings: "updateSettings"
-};
-
-export default ACTION_TYPES;
