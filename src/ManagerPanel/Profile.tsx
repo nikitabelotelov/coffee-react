ec2f65dcf94f849039cf16e957afa808971cbc8c
nikitabelotelov <nikbelotelov@mail.ru>
2019-11-04 13:27:09 +0300

    add profile container and profile component

--- src/ManagerPanel/Profile.tsx
+++ src/ManagerPanel/Profile.tsx
@@ -1,40 +1,11 @@
-import {NavLink} from "react-router-dom";
-import * as React from "react";
-import {getBackLink} from "../types";
+import {connect} from "react-redux";
+import ProfileView from "./ProfileView";
+import { IAppState } from "../reducers";
 
-const Profiles = [
-    {
-        key: 0,
-        title: 'Автоматическая'
-    },
-    {
-        key: 1,
-        title: 'Ручная'
-    },
-    {
-        key: 2,
-        title: 'Арабика'
-    },
-    {
-        key: 3,
-        title: 'Бразилия'
-    }
-]
-
-export default function Profile() {
-    return (
-        <div className='setting__profile panel_root'>
-            <ul className='setting__profile-list list-group list-group-flush'>
-                {Profiles.map((el)=>{
-                    return (<li className='list-group-item' key={el.key}>{el.title}</li>)
-                })}
-            </ul>
-            <NavLink to='profile/hand' className='manager-panel__block btn-outline-dark'>
-                Настройка по параметрам
-            </NavLink>
-            <NavLink to={getBackLink()} className='manager-panel__block btn-outline-success'>
-                Назад
-            </NavLink>
-        </div>
-    );
+const mapStateToProps = (state:IAppState) => {
+    return {...state};
 }
+
+const Profile = connect(mapStateToProps)(ProfileView);
+
+export default Profile;
---
+++ src/ManagerPanel/ProfileView.tsx
@@ -0,0 +1,40 @@
+import {NavLink} from "react-router-dom";
+import * as React from "react";
+import {getBackLink} from "../types";
+
+const Profiles = [
+    {
+        key: 0,
+        title: 'Автоматическая'
+    },
+    {
+        key: 1,
+        title: 'Ручная'
+    },
+    {
+        key: 2,
+        title: 'Арабика'
+    },
+    {
+        key: 3,
+        title: 'Бразилия'
+    }
+]
+
+export default function ProfileView() {
+    return (
+        <div className='setting__profile panel_root'>
+            <ul className='setting__profile-list list-group list-group-flush'>
+                {Profiles.map((el)=>{
+                    return (<li className='list-group-item' key={el.key}>{el.title}</li>)
+                })}
+            </ul>
+            <NavLink to='profile/hand' className='manager-panel__block btn-outline-dark'>
+                Настройка по параметрам
+            </NavLink>
+            <NavLink to={getBackLink()} className='manager-panel__block btn-outline-success'>
+                Назад
+            </NavLink>
+        </div>
+    );
+}
