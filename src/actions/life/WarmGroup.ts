0815a0d47b2e276e1c23e1dd51fe4b20247cbd46
Zuev Dmitrii <xakkez@gmail.com>
2019-11-27 21:29:02 +0300

    wifi logs

--- src/actions/life/WarmGroup.ts
+++ src/actions/life/WarmGroup.ts
@@ -45,9 +45,16 @@ export const WarmGroup = (
       }
       const current = Date.now()
       if (current - state.stepStart > state.waitWarmTimeout) {
-        state.step = '0'
+        state.step = '2'
+        state.stepStart = Date.now()
       }
       break;
+    case '2': 
+      const current2 = Date.now()
+      if (current2 - state.stepStart > 500) {
+        state.step = '0'
+      }
+      break
     default:
       if (!state.I) {
         state.I = 0;
@@ -72,18 +79,18 @@ export const WarmGroup = (
           state.waitWarmTimeout = 1000
         } else if (e < 0) {
           state.power = state.power / 2
-          if (state.power < 1000) {
+          if (state.power < 300) {
             state.power = 0
             state.waitWarmTimeout = 500
           } else {        
-            state.waitWarmTimeout = state.power > 5000 ? 5000 : state.power 
+            state.waitWarmTimeout = state.power > 700 ? 700 : state.power 
           }
         } else if (e < 1) {
           state.power = state.power / 2
-          if (state.power < 800) {
-            state.power = 800
+          if (state.power < 500) {
+            state.power = 500
           } 
-          state.waitWarmTimeout = state.power > 5000 ? 5000 : state.power 
+          state.waitWarmTimeout = state.power > 1000 ? 1000 : state.power 
         } else {
           if (state.power < 1000) {
             state.power = 1000
