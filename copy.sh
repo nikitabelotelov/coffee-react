87653127ae74d45ccab14b2e699506b365414327
nikitabelotelov <nikbelotelov@gmail.com>
2020-01-13 19:21:41 +0300

    fix react-lib deps

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-for i in dist/ font/ server.js index.html ; do cp -r $i ../coffee-build/ ; done
+for i in dist/ ext/ font/ server.js index.html ; do cp -r $i ../coffee-build/ ; done
 
