f99ea835cdbc25d4748c88599aace13a493d25a9
Zuev Dmitrii <xakkez@gmail.com>
2020-03-04 14:30:42 +0300

    add logs to serverredux

--- src/actions/serverRedux.ts
+++ src/actions/serverRedux.ts
@@ -1,7 +1,7 @@
 
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
-import Converter, { ISTMCommand } from "../../server/stm/Converter";
+import Converter, { ISTMCommand, StmCommands } from "../../server/stm/Converter";
 import Usart from "../../server/usart/Usart";
 
 const store = createStore(rootReducer);
@@ -10,8 +10,17 @@ let dump:{usart: Usart} = {usart: null}
 export const setUsart = (_us: Usart) => {
   dump.usart = _us
 }
-
+let a1 = 0
+let a2 = 0
 export const emitStm = (payload: ISTMCommand) => {
+  if (payload.id === StmCommands.ResetVolumetricG1) {
+    console.log('VOLUME 1 = ', payload.content, '         - ', a1)
+    a1++
+  }
+  if (payload.id === StmCommands.ResetVolumetricG1) {
+    console.log('VOLUME 2 = ', payload.content, '         - ', a2)
+    a2++
+  }
   dump.usart.sendMessage(Converter.toString(payload))
 }
 
